d4cfcc54a2fd24d96e46dc23d9918d07158a9cfc
Suraj Pawar <149962552+MrSurajPawar@users.noreply.github.com>
2024-02-08 20:13:39 +0530

    Add files via upload

---
+++ Array/twoSum.java
@@ -0,0 +1,22 @@
+//import java.util.*;
+
+public class twoSum {
+    public static String sum(int n, int []arr, int target) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == target) return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
+        String ans = sum(n, arr, target);
+        System.out.println("This is the answer :: " + ans);
+    }
+
+}
+

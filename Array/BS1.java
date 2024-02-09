859822ddc533588dbe4908513cecf54218c0831b
Suraj Pawar <149962552+MrSurajPawar@users.noreply.github.com>
2024-02-09 12:18:06 +0530

    Add files via upload

---
+++ Array/BS1.java
@@ -0,0 +1,26 @@
+public class BS1 {
+        public static int binarySearch(int[] nums, int target) {
+            int n = nums.length; //size of the array.
+            int low = 0, high = n - 1;
+    
+            // Perform the steps:
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (nums[mid] == target) return mid;
+                else if (target > nums[mid]) low = mid + 1;
+                else high = mid - 1;
+            }
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+            int[] a = {3, 4, 6, 7, 9, 12, 16, 17};
+            int target = 6;
+            int ind = binarySearch(a, target);
+            if (ind == -1)
+                System.out.println("The target is not present.");
+            else
+                System.out.println("The target is at index: " + ind);
+    }
+}
+

9c69135d38abaef3eb438f06806406e53e8fe6d7
Suraj Pawar <149962552+MrSurajPawar@users.noreply.github.com>
2024-02-09 16:58:39 +0530

    Add files via upload

---
+++ Array/RotatedSortedArray2.java
@@ -0,0 +1,46 @@
+public class RotatedSortedArray2  {
+    public static boolean searchInARotatedSortedArrayII(int []arr, int k) {
+        int n = arr.length; 
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            //if mid points the target
+            if (arr[mid] == k) return true;
+
+            //Edge case:
+            if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+                low = low + 1;
+                high = high - 1;
+                continue;
+            }
+
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= k && k <= arr[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else { 
+                    if (arr[mid] <= k && k <= arr[high]) {
+                    
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6};
+        int k = 3;
+        boolean ans = searchInARotatedSortedArrayII(arr, k);
+        if (ans == false)
+            System.out.println("Target is not present.");
+        else
+            System.out.println("Target is present in the array.");
+    }
+}
+

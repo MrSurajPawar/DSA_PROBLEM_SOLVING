7099c777546a91b9a656fc5e745aefc3a9e0bbe6
Suraj Pawar <149962552+MrSurajPawar@users.noreply.github.com>
2024-02-09 12:18:18 +0530

    Add files via upload

---
+++ Array/BS2.java
@@ -0,0 +1,28 @@
+public class BS2 {
+
+    public static int binarySearch(int[] nums, int low, int high, int target) {
+        if (low > high) return -1; //Base case.
+
+        int mid = (low + high) / 2;
+
+        if (nums[mid] == target) return mid;
+        else if (target > nums[mid])
+            return binarySearch(nums, mid + 1, high, target);
+        return binarySearch(nums, low, mid - 1, target);
+    }
+
+    public static int search(int[] nums, int target) {
+        return binarySearch(nums, 0, nums.length - 1, target);
+    }
+
+    public static void main(String[] args) {
+        int[] a = {3, 4, 6, 7, 9, 12, 16, 17};
+        int target = 6;
+        int ind = search(a, target);
+        if (ind == -1)
+            System.out.println("The target is not present.");
+        else
+            System.out.println("The target is at index: " + ind);
+    }
+}
+

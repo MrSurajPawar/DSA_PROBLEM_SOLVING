74cf6e5be6981c17a9b04fc9b0a04f04ea54b1c1
MrSurajPawar <surajspawar007@gmail.com>
2024-02-26 19:35:55 +0530

    Loop length in LL

---
+++ Linked_list/LoopLength.java
@@ -0,0 +1,27 @@
+public class LoopLength 
+{
+    private static int findLength(Node slow , Node fast) {
+        int countOfSlow = 1;
+        slow = slow.next;
+
+        while(slow != fast) {
+            countOfSlow++;
+            slow = slow.next;
+        }
+        return countOfSlow;
+    }
+    public static int lengthOfLoop(Node head) {
+        Node slow = head , fast = head;
+         
+        while(fast != null  && fast.next != null) {
+            slow = slow.next;
+           
+            fast = fast.next.next;
+
+            if(slow == fast) {
+                return findLength(slow ,fast);
+            }
+        }
+        return 0;
+    }
+}

bf71fcb0b48b0d3166b574d64fe2ee35753a414e
MrSurajPawar <surajspawar007@gmail.com>
2024-03-01 17:55:37 +0530

    Finding the intersection in LL

---
+++ Linked_list/IntersectionInLL.java
@@ -0,0 +1,18 @@
+public class IntersectionInLL {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            if (headA == null && headB == null) return null;
+             
+            ListNode temp1 = headA , temp2 = headB;
+    
+            while ( temp1 != temp2 ) {
+                temp1 = temp1.next;
+                temp2 = temp2.next;
+    
+                if(temp1 == temp2)  return  temp1;
+                if  (temp1 == null)  temp1 = headB;
+                if  (temp2 == null)  temp2 = headA;
+            }
+    
+            return temp1;
+        }
+    }

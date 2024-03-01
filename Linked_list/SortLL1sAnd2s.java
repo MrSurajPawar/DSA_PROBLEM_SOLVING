d71b412b826b610c6fc6563297e3280532d89424
MrSurajPawar <surajspawar007@gmail.com>
2024-03-01 17:56:18 +0530

    Sort LL 1s and 2s

---
+++ Linked_list/SortLL1sAnd2s.java
@@ -0,0 +1,35 @@
+public class SortLL1sAnd2s {
+    public static Node sortList(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node temp = head;
+        //Dummy nodes 
+        Node zeroHead = new Node(-1);
+        Node oneHead = new Node(-1);
+        Node twoHead = new Node(-1);
+
+        Node zero = zeroHead , one = oneHead , two = twoHead;
+
+        while (temp != null) {
+            if (temp.data == 0) {
+                zero.next = temp;
+                zero = zero.next;
+            } else if (temp.data == 1) {
+                one.next = temp;
+                one = one.next;
+            } else {
+                two.next = temp;
+                two = two.next;
+            }
+            temp = temp.next;
+        }
+
+        zero.next = (oneHead.next != null) ? oneHead.next : twoHead.next;
+        one.next = twoHead.next;
+        two.next = null;
+
+        Node newHead = zeroHead.next;
+        return newHead; 
+    }
+}

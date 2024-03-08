ca4e494b8aaaadc4ef0c0cd79b467dfbff517625
Suraj Pawar <149962552+MrSurajPawar@users.noreply.github.com>
2024-03-08 20:47:18 +0530

    Create removeDuplicatesDLL.cpp

---
+++ removeDuplicatesDLL.cpp
@@ -0,0 +1,19 @@
+Node * removeDuplicates(Node *head)
+{
+    Node* temp = head;
+
+    while (temp != nullptr && temp->next != nullptr) {
+        Node* nextNode = temp->next;
+        while (nextNode != nullptr && nextNode->data == temp->data ) {
+            Node* duplicates = nextNode;
+            nextNode = nextNode->next;
+            free(duplicates);
+        }
+        temp->next = nextNode;  
+        if (nextNode != nullptr)   nextNode->prev = temp;
+
+        temp = temp->next;       
+    }
+
+    return head;
+}

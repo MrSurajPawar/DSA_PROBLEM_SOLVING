34e3e09a58c0058714889b2fc6ac6295713f5830
MrSurajPawar <surajspawar007@gmail.com>
2024-03-10 19:13:19 +0530

    Rotated String

---
+++ Strings/RotatedString.java
@@ -0,0 +1,24 @@
+package Strings;
+
+public class RotatedString {
+        public boolean rotateString(String s, String goal) {
+            if (s.length() != goal.length()) {
+                return false;
+            }
+    
+            int n = s.length();
+    
+            for (int i = 0 ; i < n ; i++) {
+                String rotated = rotate(s , i);
+                if ( rotated.equals(goal) ){
+                    return true;
+                } 
+            }
+    
+            return false;
+        }
+    
+        public static String rotate( String str , int k) {
+            return str.substring(k) + str.substring(0 , k);
+        }
+    }

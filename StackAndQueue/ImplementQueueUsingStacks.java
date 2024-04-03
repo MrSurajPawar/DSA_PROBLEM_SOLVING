b86dd066b40a9a925fd5367ae55f8625498aec01
MrSurajPawar <surajspawar007@gmail.com>
2024-04-03 17:55:33 +0530

    Queue implementation using stack

---
+++ StackAndQueue/ImplementQueueUsingStacks.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+public class ImplementQueueUsingStacks {
+        Stack<Integer> s1 = new Stack();
+        Stack<Integer> s2 = new Stack();
+    
+        public void push(int x) {
+            s1.push(x);
+        }
+    
+        public int pop() {
+            peek();
+            return s2.pop();
+        }
+    
+        public int peek() {
+            if (s2.isEmpty()) {
+                while (!s1.isEmpty()) {
+                    s2.push(s1.pop());
+                }
+            }
+            return s2.peek();
+        }
+    
+        public boolean empty() {
+            return s1.isEmpty() && s2.isEmpty();
+        }
+    }

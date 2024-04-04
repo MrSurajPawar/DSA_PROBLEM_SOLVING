dbe0ad7577a74bbe39310b9cb7c07e9f59c61de1
MrSurajPawar <surajspawar007@gmail.com>
2024-04-04 17:56:20 +0530

    Implmentation of min stack

---
+++ StackAndQueue/MinStack.java
@@ -0,0 +1,35 @@
+public class MinStack {
+
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stk = new Stack();
+
+    public void push(int val) {
+        if (val <= min) {
+            stk.push(min);
+            min = val;
+        }
+        stk.push(val);
+    }
+
+    public void pop() {
+        if (stk.pop() == min)
+            min = stk.pop();
+    }
+
+    public int top() {
+        return stk.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */

a0a025af9e78af07814a0244eaa7bf59a816add0
MrSurajPawar <surajspawar007@gmail.com>
2024-04-13 10:27:37 +0530

    sliding problem

---
+++ SlidingWindowProblem/MaximumPointsCanObtainCards.java
@@ -0,0 +1,23 @@
+package SlidingWindowProblem;
+
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length;
+        int lsum = 0 , rsum = 0 , maxSum = 0;
+        int rIndex = n-1;
+
+        for (int i = 0 ; i <= k-1 ;i++)
+            lsum = lsum + cardPoints[i];
+        maxSum = lsum;
+
+        for (int i = k - 1 ; i >= 0 ; i--) {
+            lsum -= cardPoints[i];
+            rsum += cardPoints[rIndex];
+            rIndex--;
+
+            maxSum = Math.max(maxSum , lsum + rsum);
+        }
+
+        return maxSum;   
+    }   
+}

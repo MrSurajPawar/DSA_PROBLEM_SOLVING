51be5fd3c8a067d93fa1766b82aa0462873333eb
Suraj Pawar <149962552+MrSurajPawar@users.noreply.github.com>
2024-04-15 17:24:01 +0530

    Update BS1.java

--- Array/BS1.java
+++ Array/BS1.java
@@ -1,4 +1,4 @@
-public class BS1 
+public class BS1 {
         public static int binarySearch(int[] nums, int target) {
             int n = nums.length; //size of the array.
             int low = 0, high = n - 1;

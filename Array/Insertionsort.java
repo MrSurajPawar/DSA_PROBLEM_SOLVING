94978cd00d8a404d8cf496334069d5fea40cbbba
Suraj Pawar <149962552+MrSurajPawar@users.noreply.github.com>
2024-05-11 21:26:08 +0530

    Update Insertionsort.java

--- Array/Insertionsort.java
+++ Array/Insertionsort.java
@@ -4,7 +4,6 @@ public class Insertionsort {
 
         for(int i = 0 ;i <=size-1; i++){
             int j = i;
-
             while(j > 0 && nums[j-1] > nums[j]){
                 int temp = nums[j-1];
                 nums[j-1] = nums[j];
